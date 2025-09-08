dca78ffc4c484540d5a4b17ae1c2affd00e6312d
amanohina1 <amanohina1@outlook.com>
2025-09-09 00:20:21 +0800

    Update Pipeline_Regs.sv

--- Pipeline_Regs.sv
+++ Pipeline_Regs.sv
@@ -33,10 +33,10 @@ module Pipeline_Regs #(
 
     always_ff @(posedge clk) begin  
         if (rst) begin
-            if_id_reg  <= '{32'h00000013,32'b0,1'b1};
-            id_ex_reg  <= '{valid : 1'b1,default: '0};
-            ex_mem_reg <= '{valid : 1'b1,default: '0};
-            mem_wb_reg <= '{valid : 1'b1,default: '0};
+            if_id_reg  <= '{32'h00000013,32'b0,1'b0};
+            id_ex_reg  <= '{valid : 1'b0,default: '0};
+            ex_mem_reg <= '{valid : 1'b0,default: '0};
+            mem_wb_reg <= '{valid : 1'b0,default: '0};
         end else begin
 
             if (flush_if_id) begin
@@ -45,17 +45,17 @@ module Pipeline_Regs #(
             else if_id_reg  <= if_id_next;
 
             if (flush_id_ex) begin
-                id_ex_reg <= '{valid : 1'b1,default: '0};
+                id_ex_reg <= '{valid : 1'b0,default: '0};
             end else if (stall_id_ex) if_id_reg  <= if_id_reg;
             else id_ex_reg  <= id_ex_next;
             
             if (flush_ex_mem) 
-                ex_mem_reg <= '{valid : 1'b1,default: '0};
+                ex_mem_reg <= '{valid : 1'b0,default: '0};
             else if (stall_ex_mem) if_id_reg  <= if_id_reg;
             else ex_mem_reg  <= ex_mem_next;
             
             if (flush_mem_wb) 
-                mem_wb_reg <= '{valid : 1'b1,default: '0};
+                mem_wb_reg <= '{valid : 1'b0,default: '0};
             else if (stall_mem_wb) if_id_reg  <= if_id_reg;
             else mem_wb_reg  <= mem_wb_next;
 
@@ -66,5 +66,6 @@ module Pipeline_Regs #(
     assign id_ex_reg_o  = id_ex_reg;
     assign ex_mem_reg_o = ex_mem_reg;
     assign mem_wb_reg_o = mem_wb_reg;
+
 
 endmodule
